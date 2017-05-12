e34d8f779946951385827a3ee89a92590be01a66
Gregor Langner <a1502605@unet.univie.ac.at>
2017-05-12 14:54:07 +0200

    Databasehandler

---
+++ src/controller/DatabaseHandler.java
@@ -0,0 +1,79 @@
+package ;// muss noch hinzugefügt werden;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseHandler {
+
+	private static DatabaseHandler instance = null;
+
+	private static Connection connection = null;
+
+	private static final String PROPERTY_URL = "jdbc:mysql://langnerg86.mysql.univie.ac.at/langnerg86?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+	private static final String PROPERTY_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+	private static final String PROPERTY_USERNAME = "langnerg86";
+
+	private static final String PROPERTY_PASSWORD = "dbkzlxlq1";
+	private DatabaseHandler() {
+	}
+
+	public static Connection getConnection() throws SQLException {
+
+		if (connection == null) {
+
+			try {
+
+				Class.forName(PROPERTY_DRIVER);
+
+			} catch (ClassNotFoundException e) {
+
+				// LOGGER.log(Level.SEVERE, "MySQL JDBC-Driver could not be
+				// loaded!", e);
+
+			}
+
+			connection = DriverManager.getConnection(PROPERTY_URL, PROPERTY_USERNAME, PROPERTY_PASSWORD);
+
+		}
+
+		return connection;
+
+	}
+
+	public static void closeConnection() throws SQLException {
+
+		try {
+
+			if (connection != null) {
+
+				connection.close();
+
+				// LOGGER.info("DatabaseHandler: Connection was closed");
+
+			}
+
+		} catch (SQLException e) {
+
+			// LOGGER.severe("DatabaseHandler: " + e.getMessage());
+
+			throw new SQLException(e + "");
+
+		}
+
+	}
+
+	public static DatabaseHandler getInstance() {
+
+		if (instance == null) {
+
+			instance = new DatabaseHandler();
+
+		}
+
+		return instance;
+
+	}
+}
